79964a57de306f3b074aea8d7d3705bb4c7c6f68
Charles <Charlessammarco@gmail.com>
2023-04-27 09:58:44 -0500

    funct cont

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(x = 'roller-skate'){
+    return `This Saturday, I want to ${x}!`
+}
+
+function mondayWork(y = 'go to the office'){
+    return `This Monday, I will ${y}.`;
+}
+
+function wrapAdjective(s="*"){
+   return function (z="special"){
+    return `You are ${s}${z}${s}!`
+
+   }
+}
